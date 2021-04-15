b383f47be3533bf9d70e6c9cd05406ac2af6ebc6
Ernesto Iser <ernestoj.ic@gmail.com>
2021-04-15 17:44:19 -0500

    start working on the linked-list

--- data-structures/linked-list/linkedlist.go
+++ data-structures/linked-list/linkedlist.go
@@ -1,12 +1,46 @@
 package linkedlist
 
+import "fmt"
+
 // Node represents an element of the list
-type Node struct {
+type node struct {
 	data int
-	next *Node
+	next *node
 }
 
 // LinkedList is the list itself
-type LinkedList struct {
-	head *Node
+type linkedList struct {
+	head   *node
+	length int
 }
+
+// prepend func inserts a new element
+// at the begining of the list
+func (l *linkedList) prepend(data int) {
+	newNode := node{data: data, next: nil}
+	if l.head != nil {
+		newNode.next = l.head
+		l.head = &newNode
+		l.length++
+	} else {
+		l.head = &newNode
+		l.length++
+	}
+}
+
+// prints the list
+func (l *linkedList) print() {
+	if l.head == nil {
+		return
+	}
+
+	currentNode := l.head
+	for currentNode != nil {
+		fmt.Println(currentNode.data)
+		currentNode = currentNode.next
+	}
+}
+
+// func (l *linkedList) deleteWithValue(value int) {
+
+// }
